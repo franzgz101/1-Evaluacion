9ee32933b760b99dc6abc62d57264be56508471c
franzgz101 <43232417+franzgz101@users.noreply.github.com>
2018-10-22 09:45:56 +0200

    Add files via upload

---
+++ bucle_6.py
@@ -0,0 +1,10 @@
+def bucle_6():
+    print "***********************"
+    print"*TABLA DE MULTIPLICAR*"
+    print "***********************"
+    for ntbala in range(1,11):
+        print "TABLA DEL ",ntabla
+        for i in range(1,11):
+            print ntabla," 1 ",i, " = ",ntabla*i
+
+bucle_6()
