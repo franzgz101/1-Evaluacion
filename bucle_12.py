c47df7117d8aa793e17cc42becbccdb01e45386c
franzgz101 <43232417+franzgz101@users.noreply.github.com>
2018-11-22 09:22:31 +0100

    Add files via upload

---
+++ bucle_12.py
@@ -0,0 +1,7 @@
+def bucle_12():
+    print"Dime 4 numeros al azar"
+    for i in range(1,5):
+        numero=input("numero ",i,": ")
+        print "Me acabas de dar el numero" ,numero
+        
+bucle_12()
