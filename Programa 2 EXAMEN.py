ee5754715dcfa4892fd038e4bc54afd91b902529
franzgz101 <43232417+franzgz101@users.noreply.github.com>
2018-11-29 17:30:11 +0100

    Add files via upload

---
+++ Programa 2 EXAMEN.py
@@ -0,0 +1,44 @@
+def programa():
+    n1 = float(input("Enter first number: "))
+    n2 = float(input("Enter second number: "))
+    n3 = float(input("Enter third number: "))
+    n4 = float(input("Enter fourth number: "))
+    n5 = float(input("Enter fifth number: "))
+    n6 = float(input("Enter sixth number: "))
+    n7 = float(input("Enter seventh number: "))
+    n8 = float(input("Enter eigth number: "))
+    n9 = float(input("Enter ninth number: "))
+    n10 = float(input("Enter tenth number: "))
+    for x in range (10000):
+        if(n1 <=x):
+            print str(n1)
+            break
+        elif(n2 <=x):
+            print str(n2)
+            break
+        elif(n3 <=x):
+            print str(n3)
+            break
+        elif(n4 <=x):
+            print str(n4)
+            break
+        elif(n5 <=x):
+            print str(n5)
+            break
+        elif(n6 <=x):
+            print str(n6)
+            break
+        elif(n7 <=x):
+            print str(n7)
+            break
+        elif(n8 <=x):
+            print str(n8)
+            break
+        elif(n9 <=x):
+            print str(n9)
+            break
+        elif(n10 <=x):
+            print str(n10)
+            break
+
+programa()
